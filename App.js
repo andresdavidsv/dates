c204cd20f68f9ea8ef58d9d1623d24bb51505007
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-07-12 18:44:38 -0500

    :tada: Begin project "dates"

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text style={styles.title}>Administrador de Citas</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+  },
+});
+
+export default App;
