83a77bb458210859ffdb15315e064281778817e7
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-07-15 14:29:33 -0500

    :sparkles: Add select time and dates

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,41 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Form = () => {
+  const [date, saveDate] = useState('');
+  const [time, saveTime] = useState('');
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirmDate = date => {
+    const options = {year: 'numeric', month: 'long', day: '2-digit'};
+    saveDate(date.toLocaleDateString('en-US', options));
+    hideDatePicker();
+  };
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false);
+  };
+
+  const handleConfirmTime = time => {
+    const options = {hour: 'numeric', minute: '2-digit', hour12: false};
+    saveTime(time.toLocaleTimeString('en-US', options));
+    hideTimePicker();
+  };
   return (
     <>
       <View style={styles.container}>
@@ -26,6 +60,29 @@ const Form = () => {
             onChangeText={text => console.log(text)}
             keyboardType={'numeric'}
           />
+        </View>
+        <View>
+          <Text style={styles.label}>Date: </Text>
+          <Button title="Show Date Picker" onPress={showDatePicker} />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirmDate}
+            onCancel={hideDatePicker}
+          />
+          <Text>{date}</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Time: </Text>
+          <Button title="Show Time Picker" onPress={showTimePicker} />
+          <DateTimePickerModal
+            isVisible={isTimePickerVisible}
+            mode="time"
+            onConfirm={handleConfirmTime}
+            onCancel={hideTimePicker}
+            is24Hour
+          />
+          <Text>{time}</Text>
         </View>
         <View>
           <Text style={styles.label}>Sintoms: </Text>
